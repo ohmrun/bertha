0af932373225c8f07b590297b2dd7c641fdca046
0b1kn00b <polysemantic@gmail.com>
2023-03-17 13:18:54 +0100

    ok

---
+++ .local/bertha/i/src/bertha/meta/install/linux.sh
@@ -0,0 +1,6 @@
+  NEKO_VERSION_STRING="neko-2.3.0-linux64";
+  NEKO_REMOTE_FILENAME="${NEKO_VERSION_STRING}.tar.gz"
+  NEKO_TGT=$( direnv fetchurl "https://github.com/HaxeFoundation/neko/releases/download/v2-3-0/${NEKO_REMOTE_FILENAME}" "sha256-Jt2ijQpRQH8mIYupwsNVyOsjzysLYXJ0sA5LkXD+aes=")
+  #echo $NEKO_TGT;
+  mv -n "${NEKO_TGT}" "${BERTHA_PATH}/tmp/${NEKO_REMOTE_FILENAME}"
+  tar -xzf "${BERTHA_PATH}/tmp/${NEKO_REMOTE_FILENAME}" --directory "${BERTHA_PATH}/.local/bertha/o/os/linux/prg"
---
+++ README.md
@@ -0,0 +1,11 @@
+# Bertha
+
+[direnv](direnv.net) based Haxe setup using [Neko](https://nekovm.org/).
+
+Not yet tested on Mac or Windows, but intented to work there so please report issues.
+
+
+```
+git clone https://github.com/ohmrun/bertha.git
+cd bertha && direnv allow
+```
