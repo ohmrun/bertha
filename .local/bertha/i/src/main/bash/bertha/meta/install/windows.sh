d1e4ac1df6eacbea505b7ae7b9c80186464719cb
0b1kn00b <polysemantic@gmail.com>
2023-03-17 13:45:38 +0100

    get tmp into correct folder

--- .local/bertha/i/src/main/bash/bertha/meta/install/windows.sh
+++ .local/bertha/i/src/main/bash/bertha/meta/install/windows.sh
@@ -1,6 +1,6 @@
 NEKO_VERSION_STRING="$1"
 NEKO_REMOTE_FILENAME="${NEKO_VERSION_STRING}.zip"
 NEKO_TGT=$(direnv fetchurl "https://github.com/HaxeFoundation/neko/releases/download/v2-3-0/${NEKO_REMOTE_FILENAME}" "sha256-0J/fNizS4ydPbIUovnIRZjJgw6UyPOiTt2N8KBiZXws=")
-mv -n "${NEKO_TGT}" "${BERTHA_PATH}/tmp/${NEKO_REMOTE_FILENAME}"
+mv -n "${NEKO_TGT}" "${BERTHA_PATH}/.local/bertha/o/tmp/${NEKO_REMOTE_FILENAME}"
 #Windows 10 build 17063 or later 
-tar -xf "${BERTHA_PATH}/tmp/${NEKO_REMOTE_FILENAME}" --directory "${BERTHA_PATH}/.local/bertha/o/os/windows/prg"
+tar -xf "${BERTHA_PATH}/.local/bertha/o/tmp/${NEKO_REMOTE_FILENAME}" --directory "${BERTHA_PATH}/.local/bertha/o/os/windows/prg"
