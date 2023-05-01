e0c54e579369af87329b9be6dd09ef8522b32a73
0b1kn00b <polysemantic@gmail.com>
2023-05-01 15:46:14 +0200

    tinkering with transmission formats

--- .local/bertha/i/src/main/bb/bertha/util.clj
+++ .local/bertha/i/src/main/bb/bertha/util.clj
@@ -1,4 +1,14 @@
-(ns bertha.util)
+(ns bertha.util
+  (:require [cheshire.core :refer :all])
+)
+
+;; (require [cheshire.core :refer :all])
+;; (require '[com.velisco/tagged :as tag])
+;; (require '[clojure.data.json :as json])
 
 (defn localhost.os.arch [] (System/getProperty "os.arch" ))
 (defn localhost.os.name [] (System/getProperty "os.name" ))
+
+(defn device.info []
+  {:arch (localhost.os.arch) :name (localhost.os.name)}
+)
