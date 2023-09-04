b2212bea57c7ed62750a63d9ea9a4d73527a3010
0b1kn00b <polysemantic@gmail.com>
2023-09-04 09:27:36 +0200

    softlink bertha into ~/.local/share so as to determine install method

--- .local/share/bertha/i/src/main/bb/bertha/util.clj
+++ .local/share/bertha/i/src/main/bb/bertha/util.clj
@@ -1,5 +1,8 @@
 (ns bertha.util
-  (:require [cheshire.core :refer :all])
+  (
+    :require [cheshire.core :refer :all]
+    :require [babashka.fs :as fs]
+  )
 )
 
 ;; (require [cheshire.core :refer :all])
@@ -9,8 +12,10 @@
 (defn localhost.os.arch [] (System/getProperty "os.arch" ))
 (defn localhost.os.name [] (System/getProperty "os.name" ))
 (defn bertha.path [] (System/getenv "BERTHA_PATH"))
+(defn bertha.home [] (System/getenv "BERTHA_HOME"))
 
 (defn device.info []
   {:arch (localhost.os.arch) :name (localhost.os.name)}
 )
 
+(defn bertha.softlink [] (fs/create-sym-link (bertha.home) (bertha.path)))
